1a0adfff179320535d80627eb3fed38fcb371f46
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-06-20 21:36:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+
 const getSum = () => {
 //Add your code here
-  
-};
+	let x = document.querySelectorAll(".price");
+	let ttl = 0;
+	for(let i = 0;i<x.length;i++){
+		ttl += parseInt(x[i].textContent);
+	}
+	let tbl = document.querySelector('table');
+	let tr = document.createElement("tr");
+	tr.innerHTML = `
+ <td>Total Price</td>
+ <td>${ttl}</td>`
+	tbl.appendChild(tr);
+  };
 
 getSumBtn.addEventListener("click", getSum);
 
